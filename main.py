a3cbd4fbe10f2c085766653552a9eb9f8fc30e09
Mik322 <mchsa@iscte-iul.pt>
2021-03-23 16:23:34 +0000

    Changed from event to command

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import discord
 from discord.ext import commands
 import os
-from discord.message import Message
 from dotenv import load_dotenv
-import command_parser as cp
+import utils
 
 load_dotenv()
 
-client: commands.Bot = commands.Bot(command_prefix='.')
+client: commands.Bot = commands.Bot(command_prefix="$")
 client.remove_command('help')
 
 
@@ -16,15 +15,19 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-@client.event
-async def on_message(message: Message):
-    if message.author == client.user:
+@client.command(pass_context=True)
+async def sg(ctx, company=None, *args):
+    if company is None:
+        await ctx.send("No ticker was passed")
         return
-
-    if message.content.startswith('-'):
-        await cp.parse_command(message.content, message.channel)
-
-    await client.process_commands(message)
+    data, out = utils.load_data(company, **utils.parse_kwargs(args))
+    if out is not None:
+        await ctx.send(out)
+        return
+    if not utils.create_graph(data, 'Close', company):
+        await ctx.send("Stock not found")
+    else:
+        await send_image(ctx)
 
 
 @client.command(pass_context=True)
@@ -34,13 +37,18 @@ async def help(ctx):
     )
 
     embed.set_author(name="Help")
-    embed.add_field(name="-sg <ticker_symbol> <op:interval=value> <op:period=value>",
+    embed.add_field(name="$sg <ticker_symbol> <op:interval=value> <op:period=value>",
                     value="Returns a graph with the values of the selected ticker", inline=False)
     embed.add_field(
         name="period", value="Graph datetime range: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd", inline=False)
     embed.add_field(
-        name="interval", value="Time between values: 1,m2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo", inline=False)
+        name="interval", value="Time between values: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo", inline=False)
     await ctx.send(embed=embed)
 
+
+async def send_image(ctx):
+    with open('fig.png', 'rb') as f:
+        img = discord.File(f)
+        await ctx.send(file=img)
 
 client.run(os.getenv('TOKEN'))
---
+++ utils.py
@@ -0,0 +1,43 @@
+from typing import Tuple
+import matplotlib.pyplot as plt
+import yfinance as yf
+import io
+from contextlib import redirect_stdout
+from pandas.core.frame import DataFrame
+
+
+def load_data(company: str, **kwargs) -> Tuple[DataFrame, str]:
+    kwargs.setdefault("period", "1d")
+    kwargs.setdefault("interval", "1m")
+    ticker = yf.Ticker(company)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        data = ticker.history(**kwargs)
+    out = f.getvalue()
+    if out != "":
+        return None, out
+    return data, None
+
+
+def create_graph(data: DataFrame, field: str, name: str) -> bool:
+    if data.empty:
+        return False
+    try:
+        fig = data[field]
+        fig.plot()
+        plt.title(name.upper())
+        plt.ylabel("Value")
+        plt.savefig("fig.png")
+        plt.clf()
+        return True
+    except Exception:
+        return False
+
+
+def parse_kwargs(kwargs: list) -> dict:
+    dic = {}
+    for arg in kwargs:
+        arg = arg.split("=")
+        if len(arg) == 2:
+            dic[arg[0]] = arg[1]
+    return dic
